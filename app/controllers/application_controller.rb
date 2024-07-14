98175293b0aedb789b18c63c69c2147e7ee32876
Wagner Braga <wfbraga@hotmail.com>
2024-07-13 21:38:57 -0400

    Creation of API Controllers for admin and storefront
    As part of creating controllers for admin and storefront, we also
    configure concern/authenticable.rb to require user authentication and
    application_controller so devise allows sending the name during sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
+# we have created the metho configure_permitted_parameters in the application controller
+# so we user can infor name on sing up
+# the before action is called IF the devise controller is called
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+  end
 end

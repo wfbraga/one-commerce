c372975175f3cb1a360f40bbc5e2f4274026d0e3
Wagner Braga <wfbraga@hotmail.com>
2024-08-01 23:12:12 -0400

    Just some comas

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     code { Faker::Commerce.promotion_code(digits: 6) }
     status { %i[active inactive].sample }
     discount_value { range(1..99) }
-    due_date { "2024-08-01 20:52:45" }
+    due_date { '2024-08-01 20:52:45' }
   end
 end
